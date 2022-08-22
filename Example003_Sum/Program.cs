75b5975423fcab486d37ff696a8f8347af6e5156
Ulofun <rakovod1337@gmail.com>
2022-08-22 11:02:37 +0300

    add random

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberX = new Random().Next(1, 10); // 1 2 3 4 .... 9
+Console.WriteLine(numberX);
+int numberY = new Random().Next(1, 10);
+Console.WriteLine(numberY);
+int result = numberX+numberY;
+Console.WriteLine(result);
