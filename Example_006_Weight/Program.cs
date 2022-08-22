38b0ca9355e60ab7b9bd380744094274b286cfe9
Ulofun <rakovod1337@gmail.com>
2022-08-22 16:44:07 +0300

    Find max int

---
+++ Example_006_Weight/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 12561;
+int b = 23514;
+int c = 55556;
+int d = 11119;
+int e = 12128;
+int max = a;
+if(a > max) max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+Console.Write("max = ");
+Console.WriteLine(max);
