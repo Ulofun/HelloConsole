72ed2bbe94a536b2ecabb832aa02b6f2fd68b32b
Ulofun <rakovod1337@gmail.com>
2022-08-23 11:31:03 +0300

    Square exersiace

---
+++ Example_SEMINAR_001.1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int y = Convert.ToInt32(Console.ReadLine());
+if (x >= y)
+{
+    Console.WriteLine("Первое число не является квадратом второго");
+}
+else
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
