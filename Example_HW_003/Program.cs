224ec84dd5817cacaccce8a8c9a9d476f025150e
Ulofun <rakovod1337@gmail.com>
2022-08-23 13:09:06 +0300

    ТгьиукNumberN

---
+++ Example_HW_003/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача 5: Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int start = -number;
+while(start <= number)
+{
+	Console.WriteLine(start);
+	start++;
+}
