7ce3f866fbdb40921c10755a5910ee52e7ad7ebd
Ulofun <rakovod1337@gmail.com>
2022-08-23 13:23:08 +0300

    Accept

---
+++ Example_HW_006/Program.cs
@@ -0,0 +1,5 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+Console.WriteLine("Введите трехзначное число: ");
+int X = Convert.ToInt32(Console.ReadLine());
+int result = X % 10;
+Console.WriteLine("Результат: " + result);
