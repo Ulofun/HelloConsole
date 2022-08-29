fc3964d0fda74be6ef7da0e9ba30b3c1b0b403f2
Ulofun <rakovod1337@gmail.com>
2022-08-29 18:17:35 +0300

    use function for example with number

---
+++ ExampleFunction/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает трехзначное число и показывает ВТОРУЮ цифру этого числа
+//Пример решения с использованием функции!
+
+int SecondNum(int num)
+{
+    int x = num / 10;
+    x = x % 10;
+    return x;
+}
+
+
+
+Console.WriteLine("Введите трехзначное число ");
+int num = Convert.ToInt32(Console.ReadLine());
+int x = SecondNum(num);
+Console.WriteLine("Результат равен " + x);
