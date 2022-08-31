a1abe9d3e551e3ab111be1a658155aefdd284f0d
Ulofun <rakovod1337@gmail.com>
2022-08-31 14:19:27 +0300

    WeekOfDay

---
+++ DayWeek/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+void DayOfWeek(int num)
+{
+    if ( num < 1 || num > 7)
+    Console.WriteLine("Нет такого дня");
+    else if ( num == 6 || num ==7)
+    Console.WriteLine("Выходной день");
+    else
+    Console.WriteLine("Сегодня не выходной день, иди работай!");
+}
+
+Console.WriteLine("Введите день недели от 1 до 7");
+int num = Convert.ToInt32(Console.ReadLine());
+DayOfWeek(num);
