03c3fdbea0cf25d09f6dc61476abfb46d0b34c5b
Ulofun <rakovod1337@gmail.com>
2022-09-01 14:02:45 +0300

    Task 25

---
+++ HomeWork_Seminar_4.1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число А : ");
+double A = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число B , т.е степень : ");
+double B = Convert.ToDouble(Console.ReadLine());
+double result = FuncStep(A,B);
+Console.WriteLine("Результат равен = " + result);
+
+double FuncStep(double A, double B)
+{
+  double Y = (Math.Pow(A, B));
+  result = Y;
+  return result;
+  
+  //Math.Pow (A - возводимое, В - степень)
+   
+}
+
+
+
+
+
+//Math.Pow(A,B) - возвести число А в степерь В
