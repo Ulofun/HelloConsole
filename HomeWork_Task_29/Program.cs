2a6c2ddec8c9302c991ed8296d775d93282219fc
Ulofun <rakovod1337@gmail.com>
2022-09-04 17:27:58 +0300

    Task 25

---
+++ HomeWork_Task_29/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+
+
+
+void RandomArray(int[] a)
+{
+  Random randomNumber = new Random();
+  for(int i = 0; i < a.Length; i++)
+  {
+    a[i] = randomNumber.Next(0, 100);
+  
+    Console.Write(a[i] + " ");
+
+  }
+}
+
+int[] array = new int[8];
+RandomArray(array);
